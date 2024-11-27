9c8f8eeae9bc55d1bb97a7e8bd72a19aa645fb1a
José Enrique de la Torre Leyva <37634439+untalkikin@users.noreply.github.com>
2024-11-26 19:19:10 -0600

    git ignore again

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
+import express from "express"
 
 const app = express()
 
-const port = 8000
+
 
 app.get('/', (req, res) => {
         res.send('Hola mundo en express')
     }
 )
 
+const port = process.env.PORT || 4000
 
 app.listen(port, ()=>{
     console.log(`Servidoe corriendo en el puerto: ${port}`)
